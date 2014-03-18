04775be880a786522b653c7afa354efc00cd087a
Alan Zhang <alanzhang93@gmail.com>
2014-03-18 19:23:10 -0400

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # # Initial commit # # Changes to be committed: #	new file:   .idea/.name #	new file:   .idea/artifacts/CardPuller.xml #	new file:   .idea/compiler.xml #	new file:   .idea/copyright/profiles_settings.xml #	new file:   .idea/dictionaries/Alan.xml #	new file:   .idea/encodings.xml #	new file:   .idea/libraries/com_googlecode_json_simple_json_simple_1_1_1.xml #	new file:   .idea/misc.xml #	new file:   .idea/modules.xml #	new file:   .idea/scopes/scope_settings.xml #	new file:   .idea/uiDesigner.xml #	new file:   .idea/vcs.xml #	new file:   .idea/workspace.xml #	new file:   ImageSpammer.iml #	new file:   META-INF/MANIFEST.MF #	new file:   out/production/ImageSpammer/Main.class #	new file:   out/production/ImageSpammer/getImage.class #	new file:   out/production/ImageSpammer/setGUI.class #	new file:   src/Main.java #	new file:   src/getImage.java #	new file:   src/setGUI.java # # Changes not staged for commit: #	modified:   .idea/vcs.xml #	modified:   .idea/workspace.xml #

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        getImage myImage = new getImage();
+        setGUI myGUI = new setGUI();
+        //if(){
+            myImage.getImage(getLink(),getAbrev(),getLocation());
+        //}else{
+
+        //}
+
+        System.out.println("Working!");
+    }
+
+    private static String getLink(){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Set Link?");
+        String link = "http://api.mtgapi.com/v1/card/set/" + scan.nextLine().replaceAll(" ", "%20");
+
+        return link;
+    }
+
+    private static String getAbrev(){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Set Abbreviation?");
+        String pre = scan.nextLine();
+
+        return pre;
+    }
+
+    private static String getLocation(){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Where to save?");
+        String location = "D:\\MTG\\ImagesRequired\\" + scan.nextLine();
+
+        return location;
+    }
+
+}
---
+++ src/setGUI.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class setGUI extends JFrame{
+    public setGUI(){
+        setVisible(true);
+        setSize(700, 700);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("MTG Updater");
+    }
+}
