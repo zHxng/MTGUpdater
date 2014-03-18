2620e5106c027479215cd37e4d6c45ab41d2103a
Alan Zhang <alanzhang93@gmail.com>
2014-03-18 19:43:31 -0400

    Start of GUI.

--- src/setGUI.java
+++ src/setGUI.java
@@ -2,10 +2,31 @@ import javax.swing.*;
 import java.awt.*;
 
 public class setGUI extends JFrame{
+
+    JButton image;
+    JButton price;
+
     public setGUI(){
         setVisible(true);
         setSize(700, 700);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setTitle("MTG Updater");
+        setTitle("MTGUpdater");
+
+        GridBagLayout gl = new GridBagLayout();
+        setLayout(gl);
+
+        GridBagConstraints gc = new GridBagConstraints();
+
+        image = new JButton("Fetch Images");
+        gc.gridx = 0;
+        gc.gridy = 0;
+        add(image, gc);
+
+        price = new JButton("Fetch Prices");
+        gc.gridx = 0;
+        gc.gridy = 1;
+        add(price, gc);
+
+        pack();
     }
 }
