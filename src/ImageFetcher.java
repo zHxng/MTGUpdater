7d5d08fab87755131d435519d91860f2cb10094c
Alan Zhang <alanzhang93@gmail.com>
2014-03-18 21:23:05 -0400

    somewhat fixed the stupid gridbaglayout
    OOPS FORGOT CAPS

--- src/ImageFetcher.java
+++ src/ImageFetcher.java
@@ -28,45 +28,45 @@ public class ImageFetcher extends JFrame{
         GridBagConstraints gc = new GridBagConstraints();
 
         s = new JLabel("Set: ");
-        gc.gridx = 5;
+        gc.gridx = 0;
         gc.gridy = 0;
         add(s, gc);
 
         a = new JLabel("Abrev: ");
-        gc.gridx = 5;
+        gc.gridx = 0;
         gc.gridy = 1;
         add(a, gc);
 
         l = new JLabel("Save To: ");
-        gc.gridx = 5;
+        gc.gridx = 0;
         gc.gridy = 2;
         add(l, gc);
 
         set = new JTextField();
-        gc.gridx = 15;
+        gc.gridx = 5;
         gc.gridy = 0;
         set.setColumns(15);
         add(set, gc);
 
         abrev = new JTextField();
-        gc.gridx = 15;
+        gc.gridx = 5;
         gc.gridy = 1;
         abrev.setColumns(15);
         add(abrev, gc);
 
         loc = new JTextField();
-        gc.gridx = 15;
+        gc.gridx = 5;
         gc.gridy = 2;
         loc.setColumns(15);
         add(loc, gc);
 
         back = new JButton("<-- Back");
-        gc.gridx = 35;
-        gc.gridy = 50;
+        gc.gridx = 0;
+        gc.gridy = 4;
         add(back, gc);
 
         enter = new JButton("Submit");
-        gc.gridx = 15;
+        gc.gridx = 5;
         gc.gridy = 4;
         add(enter, gc);
 
