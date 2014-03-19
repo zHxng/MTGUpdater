efafaebc7f6987fd03c0a98526aadac73cb57627
lolzballs <lolzballs@teamfield3.tk>
2014-03-18 20:29:30 -0400

    Gui positioning

--- src/ImageFetcher.java
+++ src/ImageFetcher.java
@@ -3,13 +3,14 @@ import java.awt.*;
 
 public class ImageFetcher extends JFrame{
 
-    public ImageFetcher() {
+    public ImageFetcher(JFrame prevGUI) {
         setVisible(true);
         setMinimumSize(new Dimension(700,700));
         setMaximumSize(new Dimension(700,700));
         setSize((new Dimension(700,700)));
         setPreferredSize((new Dimension(700,700)));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocation(prevGUI.getX(), prevGUI.getY());
         setTitle("MTGUpdater");
 
         GridBagLayout gl = new GridBagLayout();
