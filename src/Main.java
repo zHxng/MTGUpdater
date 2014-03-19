0295587631ae1f6c2a2a82fb8f0f59c8436ff417
Alan Zhang <alanzhang93@gmail.com>
2014-03-18 21:26:42 -0400

    changed stuff

--- src/Main.java
+++ src/Main.java
@@ -5,39 +5,11 @@ public class Main {
     public static void main(String[] args) {
         SetGUI myGUI = new SetGUI();
         //if(){
-        GetImage.getImage(getLink(), getAbrev(), getLocation());
+        //GetImage.getImage();
         //}else{
 
         //}
 
         System.out.println("Working!");
     }
-
-    private static String getLink() {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Set Link?");
-        String link = "http://api.mtgapi.com/v1/card/set/" + scan.nextLine().replaceAll(" ", "%20");
-
-        return link;
-    }
-
-    private static String getAbrev() {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Set Abbreviation?");
-        String pre = scan.nextLine();
-
-        return pre;
-    }
-
-    private static String getLocation() {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Where to save?");
-        String location = "D:\\MTG\\ImagesRequired\\" + scan.nextLine();
-
-        return location;
-    }
-
 }
