a9a5c0e4c8382d869394dcb9c87b439d8288ec05
lolzballs <lolzballs@teamfield3.tk>
2014-03-19 20:56:32 -0400

    Test stuff

--- src/PriceFetcher.java
+++ src/PriceFetcher.java
@@ -2,10 +2,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
 
 public class PriceFetcher extends JFrame{
 
     JButton back;
+    JButton urlButton;
+    JTextField url;
+    JTextField cookie;
 
     public PriceFetcher(JFrame prevGUI){
         setVisible(true);
@@ -26,8 +34,29 @@ public class PriceFetcher extends JFrame{
 
         back = new JButton("<-- Back");
         gc.gridx = 0;
-        gc.gridy = 0;
+        gc.gridy = 2;
         add(back, gc);
+
+        urlButton = new JButton("Submit");
+        gc.gridx = 1;
+        gc.gridy = 2;
+        add(urlButton, gc);
+
+        url = new JTextField();
+        gc.gridx = 0;
+        gc.gridy = 0;
+        gc.gridwidth = 100;
+        url.setColumns(20);
+        url.setText("Insert Url");
+        add(url, gc);
+
+        cookie = new JTextField();
+        gc.gridx = 0;
+        gc.gridy = 1;
+        gc.gridwidth = 100;
+        cookie.setColumns(20);
+        cookie.setText("Insert Cookie");
+        add(cookie, gc);
 
         back.addActionListener(new ActionListener() {
             @Override
@@ -37,6 +66,42 @@ public class PriceFetcher extends JFrame{
             }
         });
 
+        urlButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+//                    fetchPrice();
+                    test();
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+
         pack();
     }
+
+    private void fetchPrice() throws Exception {
+        URL url = new URL(this.url.getText());
+
+        URLConnection connection = url.openConnection();
+        connection.setRequestProperty("Cookie",  this.cookie.getText());
+
+        InputStream inputStream;
+    }
+
+    private void test() throws Exception {
+        URL obj = new URL(this.url.getText());
+        URLConnection conn = obj.openConnection();
+
+        //get all headers
+        Map<String, List<String>> map = conn.getHeaderFields();
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            System.out.println("Key : " + entry.getKey() + " ,Value : " + entry.getValue());
+        }
+
+        //get header by 'key'
+        String server = conn.getHeaderField("Server");
+    }
+
 }
